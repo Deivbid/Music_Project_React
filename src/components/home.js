3d16ae075c367be9449179cc0cbe6d87d4162c7a
David Aparicio <davidaapariciobak@gmail.com>
2018-03-05 16:26:50 -0400

    First commit

---
+++ src/components/home.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react'
+import Banner from '../components/banner'
+import ArtistList from './artist_list'
+
+const URL_ARTISTS = 'http://localhost:3004/artists'
+
+class Home extends Component {
+	constructor(props){
+		super(props)
+
+		this.state = {
+			artists:'',
+		}
+	}
+
+	componentDidMount(){
+		fetch(URL_ARTISTS, {
+			method: 'GET'
+		})
+		.then(response => response.json())
+		.then(json => {
+			this.setState({
+				artists:json
+			})
+		})
+	}
+
+	render(){
+		return(
+			<div>
+				<Banner></Banner>
+				<ArtistList allArtists={this.state.artists}></ArtistList>
+			</div>
+		)
+	}
+}
+
+export default Home
